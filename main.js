68d450870b980c9e8c33060815efc2580fce761f
Salih Derin <derin.salih@gmx.de>
2026-05-07 22:07:52 +0200

    img Render

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
-let imgSet = [
-""
+const imgSet = [
+"./assets/img/pic1.jpg",
+"./assets/img/pic2.jpg",
+"./assets/img/pic3.jpg",
+"./assets/img/pic4.jpg",
+"./assets/img/pic5.jpg",
+"./assets/img/pic6.jpg",
+"./assets/img/pic7.jpg",
+"./assets/img/pic8.jpg",
+"./assets/img/pic9.jpg",
+"./assets/img/pic10.jpg",
+"./assets/img/pic11.jpg",
+"./assets/img/pic12.jpg",
 ];
+const contentRef = document.getElementById('img-collectiv');
 
 function render() {
-    let contentRef = document.getElementById('img-collectiv');
+    
     for(let i = 0; i < imgSet.length; i++) {
-        contentRef.innerHTML += imgSet
+        contentRef.innerHTML += /*html*/ `
+            <img src="${imgSet[i]}" alt="">
+        `;
     }
 }
